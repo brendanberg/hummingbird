f43f8acd2547d63a7c276fe9ca89e1e95004d26e
Brendan Berg <brendan@plusminusfive.com>
2014-06-03 10:09:19 -0700

    Initial Commit

---
+++ Hummingbird/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hummingbird
+//
+//  Created by Brendan Berg on 6/3/14.
+//  Copyright (c) 2014 NegativeZero. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
